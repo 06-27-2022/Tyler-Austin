2aab58657e0d50c355bdfd3c11e154a0a1b1ce5a
TylerSAus <108297997+TylerSAus@users.noreply.github.com>
2022-06-30 21:19:57 -0500

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,11 @@
+public class ArraySum {
+	public static void main(String[] args) {
+		int sum = add(9, 1, 13);
+		System.out.println(sum);
+	}
+	public static int add(int a, int b, int c) {
+		return a + b + c;
+		
+		
+	}
+}
